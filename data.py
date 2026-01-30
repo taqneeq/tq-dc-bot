e762c121d968dfd97d93349184cec8f3d74da3cf
KartikJain14 <info@jkartik.in>
2026-01-31 02:13:48 +0530

    prep to prod

--- data.py
+++ data.py
@@ -3,11 +3,12 @@ import pandas as pd
 import requests
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
-# Load the Excel file
-file_path = "tq-part.xlsx"
-excel_data = pd.ExcelFile(file_path)
+# Load the CSV file
+file_path = "data.csv"
+df = pd.read_csv(file_path)
 
 # Define the Discord webhook URL
 webhook_url = os.getenv("WEBHOOK_URL") or "https://discord.com/api/webhooks/1234567890/ABCDEFGHIJKL"
@@ -15,31 +16,27 @@ webhook_url = os.getenv("WEBHOOK_URL") or "https://discord.com/api/webhooks/1234
 # Function to send data via Discord webhook
 def send_to_discord(name, email, team_id):
     message = f"!register {name} {email} {team_id}"
-    print(message)
+    # print(message)
     # Uncomment to send data to Discord
-    # data = {"content": message}
-    # response = requests.post(webhook_url, json=data)
-    # if response.status_code == 204:
-    #     print(f"Successfully sent: {message}")
-    # else:
-    #     print(f"Failed to send: {message} - Status code: {response.status_code}")
+    data = {"content": message}
+    response = requests.post(webhook_url, json=data)
+    if response.status_code == 204:
+        print(f"Successfully sent: {message}")
+    else:
+        print(f"Failed to send: {message} - Status code: {response.status_code}")
 
 async def aarambh():
-    # Iterate over each sheet and send data to Discord
-    for sheet_name in excel_data.sheet_names:
-        df = pd.read_excel(file_path, sheet_name=sheet_name)
-        
-        # Drop rows with missing values in any of the required columns
-        df = df.dropna(subset=['Name', 'Email ID', 'Team name'])
-        
-        for _, row in df.iterrows():
-            name = row['Name']
-            email = row['Email ID']
-            team_id = row['Team name']
-            send_to_discord(name, email, team_id)
-            await sleep(3)  # Sleep for 3 second to avoid rate limiting
+    # Drop rows with missing values in any of the required columns
+    df_clean = df.dropna(subset=['name', 'email', 'team'])
+    
+    for _, row in df_clean.iterrows():
+        name = row['name']
+        email = row['email']
+        team_id = row['team']
+        send_to_discord(name, email, team_id)
+        await sleep(10)  # Sleep for 3 seconds to avoid rate limiting
 
 # Entry point for running the async function
 if __name__ == "__main__":
-    run(aarambh())  # This runs the async function using asyncio's event loop
+    run(aarambh())
     print("Data sent to Discord successfully!")
